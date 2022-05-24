7e603f5fb2f5a4521570baf0cc45ac2b8c551dc1
myles-ux <fistonrwanda@gmail.com>
2022-05-24 12:50:53 +0200

    status

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,17 @@
 const client = require("../index");
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  function setStatus() {
+    let status = [
+      `with ${client.guilds.cache.size} servers`,
+      `with ${client.users.cache.size} users`,
+      `with ${client.commands.size} commands`,
+    ];
+
+    let statusRotate = Math.floor(Math.random() * status.length);
+
+    client.user.setActivity(status[statusRotate], { type: "PLAYING" });
+  }
+  client.user.setStatus("dnd");
+  setInterval(setStatus, 5000), console.log(`Logged in as ${client.user.tag}!`);
 });
